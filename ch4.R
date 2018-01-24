a2e5ded7b58aefa87bd44a525928b437e99583c0
Nicky Poison <nickpoison@hotmail.com>
2018-01-23 22:22:19 -0500

    ch4.R

--- ch4.R
+++ ch4.R
@@ -8,10 +8,10 @@ x2 = 4*cos(2*pi*1:100*10/100) + 5*sin(2*pi*1:100*10/100)
 x3 = 6*cos(2*pi*1:100*40/100) + 7*sin(2*pi*1:100*40/100)
 x = x1 + x2 + x3
 par(mfrow = c(2,2), mar=c(2,2,1,0)+.5, mgp=c(1.6,.6,0))
-plot.ts(x1, ylim=c(-10,10), main=expression(omega==6/100~~~A^2==13),  panel.first=grid(lty=1))
-plot.ts(x2, ylim=c(-10,10), main=expression(omega==10/100~~~A^2==41),  panel.first=grid(lty=1))
-plot.ts(x3, ylim=c(-10,10), main=expression(omega==40/100~~~A^2==85),  panel.first=grid(lty=1))
-plot.ts(x,  ylim=c(-16,16), main="sum",  panel.first=grid(lty=1), font.main=1)
+tsplot(x1, ylim=c(-10,10), main=expression(omega==6/100~~~A^2==13) )
+tsplot(x2, ylim=c(-10,10), main=expression(omega==10/100~~~A^2==41) )
+tsplot(x3, ylim=c(-10,10), main=expression(omega==40/100~~~A^2==85) )
+tsplot(x,  ylim=c(-16,16), main="sum", font.main=1)
 dev.off()
 
 ##############
